247be30af3305ac262f8f5124dff0047afd227ce
Butters998 <przemekfaferek@wp.pl>
2022-04-26 20:32:02 +0200

    list iteration

--- src/main/java/com/example/thymeleaf_cw/CarController.java
+++ src/main/java/com/example/thymeleaf_cw/CarController.java
@@ -4,14 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class CarController {
 
     @GetMapping("/car")
     public String getCar(Model model) {
-        Car car = new Car("BMW","i8");
-        model.addAttribute("name", "Przemek");
-        model.addAttribute("car", car);
+        Car car1 = new Car("BMW","i8");
+        Car car2 = new Car("Fiat","126p");
+        Car car3 = new Car("Polonez","Caro");
+        Car car4 = new Car("Hyundai", "i30");
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        model.addAttribute("cars", cars);
+
         return "car";
     }
 }
