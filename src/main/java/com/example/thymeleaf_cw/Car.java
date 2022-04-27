cc7ed017cbddde32003d64f6be95cb2264227661
Butters998 <przemekfaferek@wp.pl>
2022-04-27 11:05:09 +0200

    deleting and updating list

--- src/main/java/com/example/thymeleaf_cw/Car.java
+++ src/main/java/com/example/thymeleaf_cw/Car.java
@@ -3,15 +3,25 @@ package com.example.thymeleaf_cw;
 public class Car {
     private String mark;
     private String model;
+    private long id;
 
-    public Car(String mark, String model) {
+    public Car(long id, String mark, String model) {
+        this.id = id;
         this.mark = mark;
         this.model = model;
+
     }
 
     public Car() {
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getMark() {
         return mark;
     }
@@ -24,6 +34,7 @@ public class Car {
         return model;
     }
 
+
     public void setModel(String model) {
         this.model = model;
     }
@@ -33,6 +44,7 @@ public class Car {
         return "Car{" +
                 "mark='" + mark + '\'' +
                 ", model='" + model + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
--- src/main/java/com/example/thymeleaf_cw/CarController.java
+++ src/main/java/com/example/thymeleaf_cw/CarController.java
@@ -2,23 +2,22 @@ package com.example.thymeleaf_cw;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Controller
 public class CarController {
     private final List<Car> cars;
 
     public CarController() {
-        Car car1 = new Car("BMW", "i8");
-        Car car2 = new Car("Fiat", "126p");
-        Car car3 = new Car("Polonez", "Caro");
-        Car car4 = new Car("Hyundai", "i30");
+        Car car1 = new Car(1,"BMW", "i8");
+        Car car2 = new Car(2,"Fiat", "126p");
+        Car car3 = new Car(3,"Polonez", "Caro");
+        Car car4 = new Car(4,"Hyundai", "i30");
         cars = new ArrayList<>();
         cars.add(car1);
         cars.add(car2);
@@ -31,13 +30,41 @@ public class CarController {
 
         model.addAttribute("cars", cars);
         model.addAttribute("newCar", new Car());
+        model.addAttribute("newCar2", new Car());
+        model.addAttribute("newCar3", new Car());
         return "car";
     }
 
-    @GetMapping("/add-car")
-    public String addCar(@RequestParam String mark, @RequestParam String model) {
-        cars.add(new Car(mark, model));
-        //return "redirect:/car";
-        return "car"; // tutaj żeby już nas nie przenosiło ale będzie sypało błędem
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car) {
+        Optional<Car> findId = cars.stream().filter(e -> e.getId() == car.getId()).findFirst();
+        if (findId.isPresent()) {
+            return "redirect:/car";
+        }
+        else
+        cars.add(car);
+        return "redirect:/car";
+    }
+
+    @PostMapping("/remove-car")
+    public String deleteCar(@ModelAttribute Car car) {
+        Optional<Car> findId = cars.stream().filter(e -> e.getId() == car.getId()).findFirst();
+        if (findId.isPresent()) {
+            cars.remove(findId.get());
+            return "redirect:/car";
+        }
+        else
+        return "redirect:/car";
+    }
+    @PostMapping("/change-car")
+    public String changeCar(@ModelAttribute Car car) {
+        Optional<Car> findId = cars.stream().filter(e -> e.getId() == car.getId()).findFirst();
+        if (findId.isPresent()) {
+            cars.remove(findId.get());
+            cars.add(car);
+            return "redirect:/car";
+        }
+        else
+            return "redirect:/car";
     }
 }
